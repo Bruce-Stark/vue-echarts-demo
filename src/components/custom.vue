3c5d42cdcf842ba16c71fcbd5c8c4ea4078f4520
Bruce-Stark <1052882558@qq.com>
2021-03-06 16:48:47 +0800

    3.6

--- src/components/custom.vue
+++ src/components/custom.vue
@@ -1,47 +1,86 @@
 <template>
-  <div class="chart">
-    <el-card>
-      <div id="avg_node_load1_chart" style="width: 1500px;height:600px;"></div>
+  <div class="serviceInfo">
+    <el-card class="chartCard" style="height:100px">
+      <h4>
+        Prometheus
+      </h4>
     </el-card>
-
-    <el-card>
-      <div id="node_load1_chart" style="width: 1500px;height:600px;"></div>
-    </el-card>
+    <el-row>
+      <el-col :span="12">
+        <el-card style="width: 600px;height: 360px">
+          <div id="node_load1_chart" style="width: 560px;height:300px;" />
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card style="width: 600px;height: 360px">
+          <div id="avg_node_load1_chart" style="width: 560px;height:300px;" />
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <el-card style="width: 600px;height: 360px">
+          <div id="process_cpu_seconds_total_chart" style="width: 560px;height:300px;" />
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card style="width: 600px;height: 360px">
+          <div id="avg_process_cpu_seconds_total_chart" style="width: 560px;height:300px;" />
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'chart',
+  name: 'Chart',
   props: {
     refresh_interval: null
   },
-  data () {
+  data() {
     return {
       avg_node_load1_data: {},
       node_load1_data: {},
-      timer: null
+      process_cpu_seconds_total_data: {},
+      avg_process_cpu_seconds_total_data: {},
+      timer: null,
+      interval: 1000
     }
   },
-  beforeDestroy () {
+  beforeDestroy() {
     clearInterval(this.timer)
   },
+  created: function() {
+
+  },
+  mounted: function() {
+    this.avg_node_load1()
+    this.node_load1()
+    this.process_cpu_seconds_total()
+    this.avg_process_cpu_seconds_total()
+  },
   methods: {
-    avg_node_load1 () {
-      let myChart = this.$echarts.init(document.getElementById('avg_node_load1_chart'))
+    avg_node_load1() {
+      const myChart = this.$echarts.init(document.getElementById('avg_node_load1_chart'))
       let localtime = Math.round(new Date().getTime() / 1000)
-      let url = 'http://192.168.0.81:9090/api/v1/query_range?query=avg%28node_load1%7B%7D%29%0A%0A&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+      let url = 'http://192.168.131.100:9090/api/v1/query_range?query=avg%28node_load1%7B%7D%29%0A%0A&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
       this.$ajax.get(url).then(response => {
         this.avg_node_load1_data = response.data.data.result[0].values
-        this.avg_node_load1_data.forEach(function (el) {
+        this.avg_node_load1_data.forEach(function(el) {
           el[0] = new Date(el[0] * 1000)
         })
-        let option = {
+        const option = {
+          title: {
+            text: '服务器内存平均负载情况'
+          },
           xAxis: {
             type: 'time'
           },
           yAxis: {
-            type: 'value'
+            type: 'value',
+            scale: true
           },
           series: [{
             data: this.avg_node_load1_data,
@@ -51,23 +90,27 @@ export default {
           }]
         }
         myChart.setOption(option)
-      }).catch(function (error) {
+      }).catch(function(error) {
         alert(error)
       })
       this.timer = setInterval(() => {
         localtime = Math.round(new Date().getTime() / 1000)
-        url = 'http://192.168.0.81:9090/api/v1/query_range?query=avg%28node_load1%7B%7D%29%0A%0A&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+        url = 'http://192.168.131.100:9090/api/v1/query_range?query=avg%28node_load1%7B%7D%29%0A%0A&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
         this.$ajax.get(url).then(response => {
           this.avg_node_load1_data = response.data.data.result[0].values
-          this.avg_node_load1_data.forEach(function (el) {
+          this.avg_node_load1_data.forEach(function(el) {
             el[0] = new Date(el[0] * 1000)
           })
-          let option = {
+          const option = {
+            title: {
+              text: '服务器内存平均负载情况'
+            },
             xAxis: {
               type: 'time'
             },
             yAxis: {
-              type: 'value'
+              type: 'value',
+              scale: true
             },
             series: [{
               data: this.avg_node_load1_data,
@@ -77,20 +120,20 @@ export default {
             }]
           }
           myChart.setOption(option)
-        }).catch(function (error) {
-        alert(error)
+        }).catch(function(error) {
+          alert(error)
         })
-      }, 2000)
+      }, this.interval)
     },
-    node_load1 () {
-      let myChart = this.$echarts.init(document.getElementById('node_load1_chart'))
+    node_load1() {
+      const myChart = this.$echarts.init(document.getElementById('node_load1_chart'))
       let localtime = Math.round(new Date().getTime() / 1000)
-      let url = 'http://192.168.0.81:9090/api/v1/query_range?query=node_load1%7B%7D&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+      let url = 'http://192.168.131.100:9090/api/v1/query_range?query=node_load1%7B%7D&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
       this.$ajax.get(url).then(response => {
         this.node_load1_data = response.data.data.result
-        let series = []
-        this.node_load1_data.forEach(function (el) {
-          el.values.forEach(function (temp) {
+        const series = []
+        this.node_load1_data.forEach(function(el) {
+          el.values.forEach(function(temp) {
             temp[0] = new Date(temp[0] * 1000)
           })
           series.push({
@@ -100,28 +143,31 @@ export default {
             smooth: true
           })
         })
-        let option = {
+        const option = {
+          title: {
+            text: '服务器内存所有负载情况'
+          },
           xAxis: {
             type: 'time'
           },
           yAxis: {
-            type: 'value'
+            type: 'value',
+            scale: true
           },
           series: series
         }
         myChart.setOption(option)
-      }).catch(function (error) {
+      }).catch(function(error) {
         alert(error)
       })
-      let flag=0
-      this.timer =  setInterval(() => {
+      this.timer = setInterval(() => {
         localtime = Math.round(new Date().getTime() / 1000)
-        url = 'http://192.168.0.81:9090/api/v1/query_range?query=node_load1%7B%7D&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+        url = 'http://192.168.131.100:9090/api/v1/query_range?query=node_load1%7B%7D&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
         this.$ajax.get(url).then(response => {
           this.node_load1_data = response.data.data.result
-          let series = []
-          this.node_load1_data.forEach(function (el) {
-            el.values.forEach(function (temp) {
+          const series = []
+          this.node_load1_data.forEach(function(el) {
+            el.values.forEach(function(temp) {
               temp[0] = new Date(temp[0] * 1000)
             })
             series.push({
@@ -131,78 +177,172 @@ export default {
               smooth: true
             })
           })
-          let option = {
+          const option = {
+            title: {
+              text: '服务器内存所有负载情况'
+            },
             xAxis: {
               type: 'time'
             },
             yAxis: {
-              type: 'value'
+              type: 'value',
+              scale: true
             },
             series: series
           }
-          flag++
-          console.log(flag)
           myChart.setOption(option)
-        }).catch(function (error) {
+        }).catch(function(error) {
           alert(error)
         })
-      }, 2000)
+      }, this.interval)
+    },
+    process_cpu_seconds_total() {
+      const myChart = this.$echarts.init(document.getElementById('process_cpu_seconds_total_chart'))
+      let localtime = Math.round(new Date().getTime() / 1000)
+      let url = 'http://192.168.131.100:9090/api/v1/query_range?query=%281+-+avg%28rate%28node_cpu_seconds_total%7Bmode%3D%22idle%22%7D%5B1m%5D%29%29+by+%28instance%29%29*100&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+      this.$ajax.get(url).then(response => {
+        this.process_cpu_seconds_total_data = response.data.data.result
+        const series = []
+        this.process_cpu_seconds_total_data.forEach(function(el) {
+          el.values.forEach(function(temp) {
+            temp[0] = new Date(temp[0] * 1000)
+          })
+          series.push({
+            data: el.values,
+            type: 'line',
+            symbol: 'none',
+            smooth: true
+          })
+        })
+        const option = {
+          title: {
+            text: '服务器CPU所有负载情况'
+          },
+          xAxis: {
+            type: 'time'
+          },
+          yAxis: {
+            type: 'value',
+            scale: true
+          },
+          series: series
+        }
+        myChart.setOption(option)
+      }).catch(function(error) {
+        alert(error)
+      })
+      this.timer = setInterval(() => {
+        localtime = Math.round(new Date().getTime() / 1000)
+        url = 'http://192.168.131.100:9090/api/v1/query_range?query=%281+-+avg%28rate%28node_cpu_seconds_total%7Bmode%3D%22idle%22%7D%5B1m%5D%29%29+by+%28instance%29%29*100&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+        this.$ajax.get(url).then(response => {
+          this.process_cpu_seconds_total_data = response.data.data.result
+          const series = []
+          this.process_cpu_seconds_total_data.forEach(function(el) {
+            el.values.forEach(function(temp) {
+              temp[0] = new Date(temp[0] * 1000)
+            })
+            series.push({
+              data: el.values,
+              type: 'line',
+              symbol: 'none',
+              smooth: true
+            })
+          })
+          const option = {
+            title: {
+              text: '服务器CPU所有负载情况'
+            },
+            xAxis: {
+              type: 'time'
+            },
+            yAxis: {
+              type: 'value',
+              scale: true
+            },
+            series: series
+          }
+          myChart.setOption(option)
+        }).catch(function(error) {
+          alert(error)
+        })
+      }, this.interval)
+    },
+    avg_process_cpu_seconds_total() {
+      const myChart = this.$echarts.init(document.getElementById('avg_process_cpu_seconds_total_chart'))
+      let localtime = Math.round(new Date().getTime() / 1000)
+      let url = 'http://192.168.131.100:9090/api/v1/query_range?query=avg%281+-+avg%28rate%28node_cpu_seconds_total%7Bmode%3D%22idle%22%7D%5B1m%5D%29%29+by+%28instance%29%29+*+100%0A&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+      this.$ajax.get(url).then(response => {
+        this.avg_process_cpu_seconds_total_data = response.data.data.result
+        const series = []
+        this.avg_process_cpu_seconds_total_data.forEach(function(el) {
+          el.values.forEach(function(temp) {
+            temp[0] = new Date(temp[0] * 1000)
+          })
+          series.push({
+            data: el.values,
+            type: 'line',
+            symbol: 'none',
+            smooth: true
+          })
+        })
+        const option = {
+          title: {
+            text: '服务器CPU平均负载情况'
+          },
+          xAxis: {
+            type: 'time'
+          },
+          yAxis: {
+            type: 'value',
+            scale: true
+          },
+          series: series
+        }
+        myChart.setOption(option)
+      }).catch(function(error) {
+        alert(error)
+      })
+      this.timer = setInterval(() => {
+        localtime = Math.round(new Date().getTime() / 1000)
+        url = 'http://192.168.131.100:9090/api/v1/query_range?query=avg%281+-+avg%28rate%28node_cpu_seconds_total%7Bmode%3D%22idle%22%7D%5B1m%5D%29%29+by+%28instance%29%29+*+100%0A&start=' + (localtime - 3600).toString() + '&end=' + localtime + '&step=14'
+        this.$ajax.get(url).then(response => {
+          this.avg_process_cpu_seconds_total_data = response.data.data.result
+          const series = []
+          this.avg_process_cpu_seconds_total_data.forEach(function(el) {
+            el.values.forEach(function(temp) {
+              temp[0] = new Date(temp[0] * 1000)
+            })
+            series.push({
+              data: el.values,
+              type: 'line',
+              symbol: 'none',
+              smooth: true
+            })
+          })
+          const option = {
+            title: {
+              text: '服务器CPU平均负载情况'
+            },
+            xAxis: {
+              type: 'time'
+            },
+            yAxis: {
+              type: 'value',
+              scale: true
+            },
+            series: series
+          }
+          myChart.setOption(option)
+        }).catch(function(error) {
+          alert(error)
+        })
+      }, this.interval)
     }
-  },
-  created: {
-
-  },
-  mounted: function () {
-    this.avg_node_load1()
-    this.node_load1()
-  },
-  watch: {
   }
 }
+
 </script>
 
 <style scoped>
-  .chart{
-    position: relative;
-    margin-top: 10%;
-    width: 100%;
-    height: 100%;
-    background-color: lavender;
-  }
-  .mapChart{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 600px;
-  }
-  .barChart{
-    margin: 10px;
-    width: 380px;
-    height: 320px;
-  }
-  .barBox{
-    position: absolute;
-    width: 400px;
-    top: 10%;
-    left: 3%;
-    z-index: 2;
-  }
-  .checker li{
-    display: inline-block;
-    margin-right: 10px;
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border: 1px solid #888;
-  }
-  .checker .set{
-    color: lavender;
-  }
-  .newbarmap{
-    position: fixed;
-    width: 200px;
-    height: 200px;
-    border: 1px solid #ccc;
-  }
+
 </style>
